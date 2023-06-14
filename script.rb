cecff7c7159af48b5c58a843aa0db66ff282fd15
ChrisClevenger <aenguscoen@gmail.com>
2023-06-14 14:57:10 -0500

    Edit code to account for nil values in chr method

--- script.rb
+++ script.rb
@@ -5,43 +5,43 @@ def caesar_cipher(string, shift_factor)
 
 ## Convert the string to an array of characters using .split
 
-characters = string.split("")
+    characters = string.split("")
 
 ## Convert characters in the array to ASCII numberical values using for each and .ord
 
-encrypted_characters = characters.map do |item| 
+    encrypted_characters = characters.map do |item| 
     ascii_value = item.ord 
 
 ## Create a method that adds the shift factor to each ord between 65..90 and 97..122
 
-    if item.match?(/[A-Z]/)
-        shifted_value = (ascii_value - 65 + shift_factor) % 26 + 65
-    elsif item.match?(/[a-z]/)
-        shifted_value = (ascii_value - 97 + shift_factor) % 26 + 97
-    else 
-        ascii_value
-    end
+        if item.match?(/[A-Z]/)  ## For upcase
+            shifted_value = (ascii_value - 65 + shift_factor) % 26 + 65
+        elsif item.match?(/[a-z]/) ## For downcase
+            shifted_value = (ascii_value - 97 + shift_factor) % 26 + 97
+        else 
+            ascii_value # For non alphabetical
+        end
 
 
 
-## Convert new ord # to chars 
+## Convert new ord # to chr. Edited chr method to account for nil
 
-    shifted_value.char 
-end 
+        shifted_value.nil? ? item : shifted_value.chr
+    end 
 
 
 
 ## Join the array of new letters 
 
-encrypted_string = encrypted_characters.join("")
+    encrypted_string = encrypted_characters.join("")
 
 
 
 ## Puts new cipher 
 
-puts encrypted_string 
+    puts encrypted_string 
 
 
 end 
 
-caesar_cipher("I love Jessica Willems!", 7)
+caesar_cipher("Test", 1)
